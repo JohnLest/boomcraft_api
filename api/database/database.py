80bf148510ac9f8893007cbc987dfebb57920f1e
JohnLest <j_schwickerath@hotmail.com>
2021-12-21 12:18:03 +0100

    change postgreSQL connection to MariaDB connection and add requirements.txt

--- api/database/database.py
+++ api/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('postgresql+psycopg2://template:123_Temp@localhost/api_template')
+engine = create_engine('mariadb+mariadbconnector://boomcraft:123_BmCraft!@192.168.0.101:3306/boomcraft_api')
 Session = sessionmaker(bind=engine)
 session = Session()
 
