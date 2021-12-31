a1e3600fdd32c86f96bf9deca6d31127ed7d2a37
JohnLest <j_schwickerath@hotmail.com>
2021-12-31 14:11:38 +0100

    create infinite loop in a thread for update the gold rate

--- api/app/main.py
+++ api/app/main.py
@@ -1,14 +1,23 @@
+import time
+import threading
 from app.routers.userRouter import route as r_usr
-from app.routers.resourceRouter import route as r_ress
+from app.routers.resourceRouter import route as r_res
 from app.routers.helloRouter import route as r_hello
 from bll.services.goldService import *
 
 
+def loop_gold_rate():
+    gold = GoldService()
+    while True:
+        gold.update_gold_rate()
+        time.sleep(3600)
+
+
 def main(app):
     app.include_router(r_usr)
-    app.include_router(r_ress)
+    app.include_router(r_res)
     app.include_router(r_hello)
-    gold = GoldService()
-    val = gold.get_gold_rate()
+    thread = threading.Thread(target=loop_gold_rate)
+    thread.start()
 
 
--- api/bll/services/goldService.py
+++ api/bll/services/goldService.py
@@ -2,6 +2,7 @@ import requests
 from database.database import session
 from dal.repository.weight_resourceRepo import WeightResourceRepo
 from database.weight_resource import WeightResource
+from datetime import datetime
 
 
 
@@ -13,11 +14,13 @@ class GoldService:
         self.endpoint = 'carat'
         self.access_key = 'iu11miori1q84ubzp0o28j73qp99v2gxy55vw9p7yu1jrm58n4t3mkofa003'
 
-    def get_gold_rate(self):
+    def update_gold_rate(self):
         resp = requests.get(
             'https://metals-api.com/api/'+self.endpoint+'?access_key='+self.access_key)
         resp_json = resp.json()
         rate_gram = int((resp_json["rates"]["Carat 24K"]*5)*100)
+        print(resp_json["timestamp"])
+        print(datetime.fromtimestamp(resp_json["timestamp"]))
         print(rate_gram)
         self.weight_res_repo.update(update={WeightResource.weight: rate_gram},
                                     filter=WeightResource.id_name_res == 5)
