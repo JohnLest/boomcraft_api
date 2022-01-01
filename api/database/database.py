f1b1cfcee3d2e3209d0f5aee54614727d531f377
JohnLest <j_schwickerath@hotmail.com>
2022-01-01 15:07:12 +0100

    change url for connect to boomcraft_api DB

--- api/database/database.py
+++ api/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('mariadb+mariadbconnector://boomcraft:123_BmCraft@192.168.0.101:3307/boomcraft_api')
+engine = create_engine('mariadb+mariadbconnector://root:ettuobel1970!@192.113.50.7:40000/boomcraft_api')
 Session = sessionmaker(bind=engine)
 session = Session()
 
