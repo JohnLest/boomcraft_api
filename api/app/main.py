5a3e1bfa0aa0d82fc9e2c421571be28f77ae12ad
JohnLest <j_schwickerath@hotmail.com>
2022-01-21 10:21:34 +0100

    resolve bug update session

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,6 @@
 import time
 import threading
+from starlette.responses import RedirectResponse
 from app.routers.userRouter import route as r_usr
 from app.routers.resourceRouter import route as r_res
 from bll.services.goldService import *
@@ -17,5 +18,9 @@ def main(app):
     app.include_router(r_res)
     thread = threading.Thread(target=loop_gold_rate)
     thread.start()
+
+    @app.get('/')
+    def hello():
+        return RedirectResponse(url='/docs')
 
 
