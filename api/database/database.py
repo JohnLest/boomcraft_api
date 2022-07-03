217494a8c5db84f3eaff87b0a658046db361e622
Jean Schwickerath <j_schwickerath@hotmail.com>
2022-07-03 12:19:50 +0200

    test
    test

--- api/database/database.py
+++ api/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('mariadb+mariadbconnector://boomcraft:123_BmCraft@192.168.0.101:3307/boomcraft_api')
+engine = create_engine('mariadb+mariadbconnector://boomcraft:123_BmCraft@192.168.0.50:3307/boomcraft_api')
 Session = sessionmaker(bind=engine)
 
 
